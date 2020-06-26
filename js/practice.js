983e76f575469a8c8875dbf4c859e5b09236e8ae
DmytroChekun <snashd4@gmail.com>
2020-06-26 19:16:09 +0300

    finished

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,7 @@
 // Color pick script
 $('.color-pick').click(function () {
     $(this).find('.color-pick__dropdown-content').slideToggle(300);
-    $(this).find('.color-pick__arrow-ico').toggleClass('rotate');
+    $('.color-pick__arrow-ico').toggleClass('rotate');
 });
 $('.color-pick__color').click(function () {
     $(this).parents('.color-pick').find('.color-pick__selected').text($(this).text());
@@ -10,33 +10,29 @@ $('.product-card').mouseleave(function(){
     $(this).find('.color-pick__dropdown-content').slideUp(300);
 });
 
-
-
-let counter = $('.amount-buy-section').attr("counter").val;
-let pickVal = $('.amount-pick__value').text;
-// $('.amount-pick__value').text = counter;
-// Counter script
-// let counter = 1;
-
-// let price = $('.price-num').val()*counter;
+// Amount pick script
 $('.amount-pick__minus').click(function () {
-    console.log($(this).parents('.amount-buy-section').attr("counter").val);
-    if ($(this).parents(counter > 1)){
-        $(this).parents(counter--);
-        $(this).find('.amount-pick__value').text($(this).parents('.amount-buy-section').attr("counter").val);
-        // $('.price-num').text(price);
+    let counter = $(this).siblings('.amount-pick__value').text();
+    if (counter > 1){
+        counter--;
+        $(this).siblings('.amount-pick__value').text(counter);
+        let price = $(this).parents('.price').find('.price-tag').text();
+        console.log(price);
     }
 })
+
 $('.amount-pick__plus').click(function () {
-    $(this).parents(counter++);
-    $(this).siblings('.amount-pick__value').text($(this).parents(counter));
-    // $('.price-num').val(price);
+    let counter = $(this).siblings('.amount-pick__value').text();
+    counter++;
+    $(this).siblings('.amount-pick__value').text(counter);
+    let price = $(this).parents('.price').find('.price-tag').text();
+    console.log(price);
 })
 
 
 // Backet ico changer
 $('.product-card__basket').click(function () {
-    $('.product-card__basket').html('<svg width="13" height="9" viewBox="0 0 13 9" fill="none">'+
-    '<path d="M12.5992 0.37656C12.0655 -0.12552 11.1979 -0.12552 10.6642 0.37656L4.78943 5.89559L2.33578 3.5909C1.80209 3.08882 0.934485 3.08882 0.400787 3.5909C-0.133596 4.09298 -0.133596 4.90685 0.400787 5.40893L3.82193 8.62328C4.08878 8.87464 4.4391 9 4.78943 9C5.13975 9 5.49008 8.87464 5.75693 8.62328L12.5992 2.19459C13.1336 1.69251 13.1336 0.878641 12.5992 0.37656Z" fill="#87AC33"/>'+'</svg>')
     
+    $(this).html('<svg width="13" height="9" viewBox="0 0 13 9" fill="none">'+
+    '<path d="M12.5992 0.37656C12.0655 -0.12552 11.1979 -0.12552 10.6642 0.37656L4.78943 5.89559L2.33578 3.5909C1.80209 3.08882 0.934485 3.08882 0.400787 3.5909C-0.133596 4.09298 -0.133596 4.90685 0.400787 5.40893L3.82193 8.62328C4.08878 8.87464 4.4391 9 4.78943 9C5.13975 9 5.49008 8.87464 5.75693 8.62328L12.5992 2.19459C13.1336 1.69251 13.1336 0.878641 12.5992 0.37656Z" fill="#87AC33"/>'+'</svg>')
 })
